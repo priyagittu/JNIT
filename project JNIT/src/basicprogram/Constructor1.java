3069c77979126e318a44cb191e28039697451861
priya <chandel.priya08@gmail.com>
2021-10-17 21:43:32 -0400

    Corrected Constructor

--- project JNIT/src/basicprogram/Constructor1.java
+++ project JNIT/src/basicprogram/Constructor1.java
@@ -1,6 +1,20 @@
 package basicprogram;
-
+class Animal {
+	int age;
+	double height;
+	String name;
+	String nature;
+	
+	Animal (int age,double height,String name,String nature){
+		this.age= age;
+		this.height= height;
+		this.name= name;
+		this.nature= nature;
+		
+	}
+}
 public class Constructor1 {
+	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
