4e266c33de133d4ec68cae03543c5caf20e4b413
priya <chandel.priya08@gmail.com>
2021-10-21 22:14:04 -0400

    done1

---
+++ project JNIT/src/basicModifier/EvenOdd.java
@@ -0,0 +1,17 @@
+package basicModifier;
+
+public class EvenOdd {
+	 int num=9;
+	
+	public static void main(String[] args) {
+		 int num=9;
+		if(num%2==0) {
+			System.out.println("number is even number" + num );
+		}
+		else 
+			System.out.println("number is odd number" + num);
+
+	}
+}
+
+
---
+++ project JNIT/src/basicModifier/LeapYear.java
@@ -0,0 +1,14 @@
+package basicModifier;
+
+public class LeapYear {
+public static void main(String[] args) {
+int year=2020;
+if(year%4==0) {
+	System.out.println("2020 is a leap year");
+	
+}
+else
+	System.out.println("2020 is not a leap year");
+	}
+
+}
