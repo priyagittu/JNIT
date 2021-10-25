6a9de303acb263edca8e67ed775090c7a3c82e50
priya <chandel.priya08@gmail.com>
2021-10-25 13:01:13 -0400

    modified

---
+++ project JNIT/src/basicModifier/IfElseExample1.java
@@ -0,0 +1,19 @@
+package basicModifier;
+
+public class IfElseExample1 {
+	public static void main(String[] args) {
+		int age=55;
+		if (age>18  &&  age<40) {
+			System.out.println(" person is young");
+		}
+		else if(age>1 && age<18) {
+				System.out.println("person is teen");
+				}
+				else 
+				System.out.println("person is old");
+				}
+				
+			
+	}
+
+
---
+++ project JNIT/src/basicModifier/Operator1.java
@@ -0,0 +1,18 @@
+package basicModifier;
+
+public class Operator1 {
+public static void main(String[] args) {
+		int result=7+1;
+	System.out.println("sum of two number is:"+result);
+	
+	result=result-1;
+	System.out.println("subtraction of two number is:"+result);	
+	result=result*2;
+	System.out.println("multiply of two number is:"+result);	
+	result=result/3;
+	System.out.println("divided of two number is:"+result);	
+	result=result%2;
+	System.out.println("modulo of two number is:"+result);
+	}
+
+}
