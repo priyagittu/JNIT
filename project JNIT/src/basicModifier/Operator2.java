72863dad3e9bdec370c29dd321e6754f9a15c8cd
priya <chandel.priya08@gmail.com>
2021-10-25 21:01:03 -0400

    done

---
+++ project JNIT/src/basicModifier/Operator2.java
@@ -0,0 +1,23 @@
+package basicModifier;
+
+public class Operator2 {
+
+	public static void main(String[] args) {
+		int x=4;
+		x++;
+	    System.out.println("increment of x is:"+x); 
+		x--;
+        System.out.println("decrement of x is:"+ --x); 
+		--x;
+		System.out.println("increment of x is:"+x);
+		++x;
+		System.out.println("decrement of x is:"+x);
+		 x=x+1;
+		System.out.println("increment of x is:"+x);
+		 x=x-1;
+		System.out.println("decrement of x is:"+x);
+		 
+
+	}
+
+}
---
+++ project JNIT/src/basicModifier/String1.java
@@ -0,0 +1,23 @@
+package basicModifier;
+
+public class String1 {
+
+	public static void main(String[] args) {
+		String priya="priya";
+		System.out.println("no of char in priya is:" + priya.length());
+String res1="hello " ;
+String res2="world" ;
+String res3=res1.concat(res2);
+System.out.println("res3 value is :" + res3 );
+	
+	String first1="java programming";
+	String second="java programming";
+	String third="python programming";
+	boolean result1=first1.equals(second);{
+	System.out.println("first1 and second:" + result1);
+}
+	boolean result2=first1.equals(third);
+	{
+		System.out.println("first1 and third:" + result2);	
+	}
+	}}
